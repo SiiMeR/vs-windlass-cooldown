5d11b4a20da6fe2b3257b1f1ad3579cabce1835b
Siim Raudsepp <siimraudsepp@msn.com>
2025-04-26 19:07:36 +0300

    Project files

---
+++ NoWindlassSpam/NoWindlassSpam/CrossbowShootPatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CombatOverhaul.Inputs;
+using CombatOverhaul.RangedSystems;
+using Crossbows;
+using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Server;
+
+namespace NoWindlassSpam;
+
+public static class CrossbowShootPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CrossbowClient), "Shoot")]
+    public static bool ShootClient(
+        CrossbowClient __instance,
+        ref bool __result,
+        ItemSlot slot,
+        EntityPlayer player,
+        ref int state,
+        ActionEventData eventData,
+        bool mainHand,
+        AttackDirection direction)
+    {
+        var cooldown = player.WatchedAttributes.GetLong("crossbowcooldown");
+        var nowSeconds = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        if (cooldown == 0L || nowSeconds > cooldown)
+        {
+            return true;
+        }
+
+        (player.Api as ICoreClientAPI).TriggerIngameError(__instance, "crossbowOnCooldown",
+            $"Cannot shoot for another {cooldown - nowSeconds:0} second(s)");
+
+        __result = false;
+        return false;
+    }
+
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CrossbowServer), "Shoot")]
+    public static void ShootServer(
+        CrossbowServer __instance, ref bool __result, IServerPlayer player, ItemSlot slot, ShotPacket packet,
+        Entity shooter)
+    {
+        if (__result)
+        {
+            var nowSeconds = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            player.Entity.WatchedAttributes.SetLong("crossbowcooldown", nowSeconds + 12);
+        }
+    }
+}
---
+++ NoWindlassSpam/NoWindlassSpam/NoWindlassSpamModSystem.cs
@@ -0,0 +1,33 @@
+﻿using Crossbows;
+using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace NoWindlassSpam;
+
+public class NoWindlassSpamModSystem : ModSystem
+{
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        var harmony = new Harmony(Mod.Info.ModID);
+
+        var original =
+            AccessTools.Method(typeof(CrossbowClient), "Shoot");
+        var patch = AccessTools.Method(typeof(CrossbowShootPatch), nameof(CrossbowShootPatch.ShootClient));
+
+        harmony.Patch(original, new HarmonyMethod(patch));
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        var harmony = new Harmony(Mod.Info.ModID);
+
+
+        var original =
+            AccessTools.Method(typeof(CrossbowServer), "Shoot");
+        var patch = AccessTools.Method(typeof(CrossbowShootPatch), nameof(CrossbowShootPatch.ShootServer));
+
+        harmony.Patch(original, postfix: new HarmonyMethod(patch));
+    }
+}
